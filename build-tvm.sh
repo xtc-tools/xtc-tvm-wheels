dded26379ddc5c77dc9df7a4eb21a299c33c1ede
Christophe Guillon <christophe.guillon@inria.fr>
2025-08-19 23:43:57 +0200

    Add support for bundling libLLVM.so correctly

--- build-tvm.sh
+++ build-tvm.sh
@@ -5,15 +5,26 @@ dir="$(realpath -e "$(dirname "$0")")"
 
 PYTHON=/opt/python/cp310-cp310/bin/python
 LLVM_PREFIX="$("$PYTHON" -c 'import llvm;print(llvm.__path__[0])')"
-LLVM_CONFIG="$LLVM_PREFIX/bin/llvm-config"
+
+TVM_INSTALL_PREFIX="${TVM_INSTALL_PREFIX-/usr/local}"
 
 cd tvm
-mkdir build
+rm -rf build "$TVM_INSTALL_PREFIX"
+mkdir -p build "$TVM_INSTALL_PREFIX"
+
+# Install llvm components in install dir
+cp -a "$LLVM_PREFIX"/lib "$LLVM_PREFIX"/include "$LLVM_PREFIX"/bin "$TVM_INSTALL_PREFIX"
+LLVM_CONFIG="$TVM_INSTALL_PREFIX/bin/llvm-config"
+
 cp cmake/config.cmake build
 sed -i \
     "s|USE_LLVM OFF|USE_LLVM $LLVM_CONFIG|" \
     build/config.cmake
 
-cmake -B build -G Ninja .
+cmake \
+    -DCMAKE_INSTALL_PREFIX:PATH="$TVM_INSTALL_PREFIX" \
+    -DCMAKE_INSTALL_RPATH='$ORIGIN' \
+    -B build -G Ninja .
 
 ninja -C build
+ninja -C build install
--- build-wheels.sh
+++ build-wheels.sh
@@ -7,6 +7,13 @@ cd "$dir"
 
 TVM_PKG_VERSION="$(cat tvm_version.txt)"
 
+# Trick for installing libLLVM along libtvm.so
+# Set TVM_LIBRARY_PATH and install tvm with ninja install
+# Set LLVM_LIB_LIST along with corresponding patch to copy libLLVM.so
+TVM_INSTALL_PREFIX="/project/tvm/install"
+TVM_LIBRARY_PATH="$TVM_INSTALL_PREFIX/lib"
+TVM_EXTRA_LIB_LIST="$TVM_INSTALL_PREFIX/lib/libLLVM.so"
+
 env \
     CIBW_PLATFORM='linux' \
     CIBW_ARCHS='x86_64' \
@@ -16,9 +23,13 @@ env \
     CIBW_BEFORE_ALL='./install-build-tools.sh && ./install-llvm.sh && ./build-tvm.sh' \
     CIBW_TEST_COMMAND='{project}/tests/test-graph.sh' \
     TVM_PKG_VERSION="$TVM_PKG_VERSION" \
-    CIBW_ENVIRONMENT_PASS_LINUX="TVM_PKG_VERSION" \
+    TVM_INSTALL_PREFIX="$TVM_INSTALL_PREFIX" \
+    TVM_LIBRARY_PATH="$TVM_LIBRARY_PATH" \
+    TVM_EXTRA_LIB_LIST="$TVM_EXTRA_LIB_LIST" \
+    CIBW_ENVIRONMENT_PASS_LINUX="TVM_PKG_VERSION TVM_INSTALL_PREFIX TVM_LIBRARY_PATH TVM_EXTRA_LIB_LIST" \
     cibuildwheel \
     tvm/python
 
+# For debug:
 #    CIBW_DEBUG_KEEP_CONTAINER=1 \
 #    CIBW_BUILD_VERBOSITY=1 \
