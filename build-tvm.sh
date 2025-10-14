a88efd63ecec4ebb37586a384a76cb5d9f224c9d
Christophe Guillon <christophe.guillon@inria.fr>
2025-10-14 13:31:05 +0200

    Update to llvm 21.1.2

--- build-tvm.sh
+++ build-tvm.sh
@@ -3,28 +3,55 @@ set -euo pipefail
 set -x
 dir="$(realpath -e "$(dirname "$0")")"
 
+# dump env                                                                                                                                                                                                        env
+
+BUILD_DIR="tvm/build"
+INSTALL_DIR="${TVM_INSTALL_PREFIX-$dir/install}"
+
+BUILD_TVM_CLEAN_BUILD_DIR="${BUILD_TVM_CLEAN_BUILD_DIR:-1}"
+BUILD_TVM_CLEAN_BUILD_DIR_POST="${BUILD_TVM_CLEAN_BUILD_DIR_POST:-0}"
+BUILD_TVM_CCACHE="${BUILD_TVM_CCACHE:-1}"
+
+TVM_BUILD_TYPE="Release" # "MinSizeRel"                                                                                                                                                                           
+
+CCACHE_OPTS=""
+[ "$BUILD_TVM_CCACHE" != 1 ] || \
+    CCACHE_OPTS="-DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+
+
 PYTHON=/opt/python/cp310-cp310/bin/python
 LLVM_PREFIX="$("$PYTHON" -c 'import llvm;print(llvm.__path__[0])')"
 
-TVM_INSTALL_PREFIX="${TVM_INSTALL_PREFIX-/usr/local}"
+[ "$BUILD_TVM_CLEAN_BUILD_DIR" != 1 ] || rm -rf "$BUILD_DIR"
 
-cd tvm
-rm -rf build "$TVM_INSTALL_PREFIX"
-mkdir -p build "$TVM_INSTALL_PREFIX"
+rm -rf "$INSTALL_DIR"
+mkdir -p "$INSTALL_DIR"
+mkdir -p "$BUILD_DIR"
+
+cd "$BUILD_DIR"
 
 # Install llvm components in install dir
-cp -a "$LLVM_PREFIX"/lib "$LLVM_PREFIX"/include "$LLVM_PREFIX"/bin "$TVM_INSTALL_PREFIX"
-LLVM_CONFIG="$TVM_INSTALL_PREFIX/bin/llvm-config"
+cp -a "$LLVM_PREFIX"/lib "$LLVM_PREFIX"/include "$LLVM_PREFIX"/bin "$INSTALL_DIR"
+LLVM_CONFIG="$INSTALL_DIR/bin/llvm-config"
 
-cp cmake/config.cmake build
+cp "$dir"/tvm/cmake/config.cmake .
 sed -i \
     "s|USE_LLVM OFF|USE_LLVM $LLVM_CONFIG|" \
-    build/config.cmake
+    config.cmake
 
 cmake \
-    -DCMAKE_INSTALL_PREFIX:PATH="$TVM_INSTALL_PREFIX" \
+    -DCMAKE_INSTALL_PREFIX:PATH="$INSTALL_DIR" \
     -DCMAKE_INSTALL_RPATH='$ORIGIN' \
-    -B build -G Ninja .
+    -DCMAKE_BUILD_TYPE="$TVM_BUILD_TYPE" \
+    $CCACHE_OPTS \
+    -Wno-dev \
+    -Wno-deprecated \
+    -G Ninja \
+    "$dir"/tvm
 
-ninja -C build
-ninja -C build install
+
+ninja
+ninja install
+
+cd "$dir"
+[ "$BUILD_TVM_CLEAN_BUILD_DIR_POST" != 1 ] || rm -rf "$BUILD_DIR"
--- install-build-tools.sh
+++ install-build-tools.sh
@@ -2,16 +2,27 @@
 set -euo pipefail
 set -x
 
+INSTALL_PREFIX="${INSTALL_PREFIX:-/usr/local}"
+CCACHE_VERSION=4.10.2
+CCACHE_URL="https://github.com/ccache/ccache/releases/download/v$CCACHE_VERSION/ccache-$CCACHE_VERSION-linux-x86_64.tar.xz"
+
 NINJA_VERSION=1.12.1
 NINJA_URL="https://github.com/ninja-build/ninja/releases/download/v$NINJA_VERSION/ninja-linux.zip"
 
-env
-
+mkdir -p "$INSTALL_PREFIX/bin"
 mkdir tmp.ninja
 cd tmp.ninja
 curl -L -o ninja-linux.zip "$NINJA_URL"
 unzip ninja-linux.zip
-cp ninja /usr/local/bin
-chmod +x /usr/local/bin/ninja
 cd ..
+cp tmp.ninja/ninja "$INSTALL_PREFIX/bin"
+chmod +x "$INSTALL_PREFIX/bin"/ninja
 rm -rf tmp.ninja
+
+mkdir tmp.ccache
+cd tmp.ccache
+curl -L -o ccache.tar.xz "$CCACHE_URL"
+tar xvJf ccache.tar.xz
+cd ..
+cp tmp.ccache/ccache*/ccache "$INSTALL_PREFIX/bin"
+rm -rf tmp.ccache
