10210cbddf50073b522caa4ec6ea9afc4c19638a
Christophe Guillon <christophe.guillon@inria.fr>
2026-02-18 14:50:04 +0100

    Use git for patches

--- checkout-tvm.sh
+++ checkout-tvm.sh
@@ -8,14 +8,18 @@ TVM_REVISION="$(cat "$dir"/tvm_revision.txt)"
 mkdir -p tvm
 cd tvm
 git init
+git config --local user.email "CIBOT@noreply.com"
+git config --local user.name "CI BOT"
 git remote add origin https://github.com/apache/tvm
 git fetch --depth 1 origin "$TVM_REVISION"
 git reset --hard FETCH_HEAD
 git submodule init
 git submodule update --recursive --depth 1
 
+# Apply patches with git and reset to fetched revision
 if [ -d "$dir"/patches ]; then
     for patch in "$dir"/patches/*.patch; do
-        patch -p1 -i "$patch"
+        git am "$patch"
     done
 fi
+git reset FETCH_HEAD
