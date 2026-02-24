05e813654bd8f0c42e247b63e98ab50dd7bd53d5
Christophe Guillon <christophe.guillon@inria.fr>
2026-02-24 15:27:58 +0100

    Update llvm tools version to 21.1.2.6

--- install-llvm.sh
+++ install-llvm.sh
@@ -5,7 +5,7 @@ dir="$(dirname "$(readlink -f "$0")")"
 
 PYTHON="${PYTHON-python}"
 INDEX_URL=https://pypi.org/simple
-LLVM_VERSION=21.1.2.5
+LLVM_VERSION=21.1.2.6
 
 $PYTHON -m pip install \
         "xtc-llvm-tools==$LLVM_VERSION" \
