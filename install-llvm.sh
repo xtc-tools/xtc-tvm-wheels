9ac31fecbaa6cacee77d57f2348403587e79e887
Liam Semeria <liamsemeria@gmail.com>
2026-07-15 15:52:21 +0200

    Update llvm tools version to 22.1.8.1

--- install-llvm.sh
+++ install-llvm.sh
@@ -5,7 +5,7 @@ dir="$(dirname "$(readlink -f "$0")")"
 
 PYTHON="${PYTHON-python}"
 INDEX_URL=https://pypi.org/simple
-LLVM_VERSION=21.1.2.6
+LLVM_VERSION=22.1.8.1
 
 $PYTHON -m pip install \
         "xtc-llvm-tools==$LLVM_VERSION" \
